9d6e537f910cda2c778461af43454a0dc2b583cf
Fairies-Feast <86023563+Fairies-Feast@users.noreply.github.com>
2024-01-06 17:02:25 -0600

    Create desmos.js

---
+++ desmos.js
@@ -0,0 +1,5 @@
+document.body.innerHTML += `<button style='postition:fixed;right:0;top:0;' onclick='this.style.display="block"'>Fetch origin</button>`;
+// DesmosJS by Amsilla.com
+// use the bookmarklet:
+// javascript:!function(){var v=document.createElement("script");v.src="https://fairies-feast.github.io/desmos.js",document.body.appendChild(v)}();;
+// and click it on Desmos
