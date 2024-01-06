a3c79fd455b7ff75266c60d5bc691da25b419745
Fairies-Feast <86023563+Fairies-Feast@users.noreply.github.com>
2024-01-06 17:06:53 -0600

    Update desmos.js

--- desmos.js
+++ desmos.js
@@ -1,4 +1,4 @@
-document.body.innerHTML += `<button style='postition:fixed;right:0;top:0;' onclick='this.style.display="block"'>Fetch origin</button>`;
+document.body.innerHTML += `<button style='postition:fixed;right:0;top:0;z-index:1000;' onclick='this.style.display="block"'>Fetch origin</button>`;
 // DesmosJS by Amsilla.com
 // use the bookmarklet:
 // javascript:!function(){var v=document.createElement("script");v.src="https://fairies-feast.github.io/desmos.js",document.body.appendChild(v)}();;
